d65119b330d45426fda9e4d94b8109d08573106a
Myo Min Latt <myominlatt.sbs@gmail.com>
2022-06-16 10:23:50 +0630

    cache: without provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:dio_flutter_transformer2/dio_flutter_transformer2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_restapi/dio_cache_inter/dio_cache_data.dart';
 import 'package:get/get.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'dio_cache_inter/dio_cache_data.dart';
 import 'interceptor_dio/app_path_provider.dart';
 import 'screen/home_screen.dart';
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized()
-//   await AppPathProvider.initPath(); //initiallize path for offline cache
-
-//   runApp(
-//     ProviderScope(
-//         child: const MyApp(),
-//     ),
-//   );
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppPathProvider.initPath(); //initiallize path for offline cache
   runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
@@ -36,11 +21,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Dio _dio = Dio();
-    // final ApiService apiService = Get.put(ApiService());
-    // _dio.interceptors.add(DioCacheInterceptor(options: options));
-    // dio.interceptors.add(
-    //     DioCacheManager(CacheConfig(baseUrl: "https://restcountries.com/v2/"))
-    //         .interceptor);
     _dio.transformer = FlutterTransformer(); // for heavy json
     dio.interceptors
         .add(LogInterceptor(logPrint: (log) => print(log), requestBody: false));
